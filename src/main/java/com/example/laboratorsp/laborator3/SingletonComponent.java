8291928a6d2a494f74b3bb58ad85cbe7498cb198
FurduiBogdanIoan <bogdan.furdui04@e-uvt.ro>
2025-10-23 17:39:34 +0300

    lab3_rezolvare

---
+++ src/main/java/com/example/laboratorsp/laborator3/SingletonComponent.java
@@ -0,0 +1,17 @@
+package com.example.laboratorsp.laborator3;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SingletonComponent {
+    public SingletonComponent() {
+        System.out.println(
+                "SingletonComponent::SingletonComponent = " + this);
+
+    }
+    public void operation() {
+        System.out.println(
+                "Invoked SingletonComponent::operation() on " + this);
+
+    }
+}
